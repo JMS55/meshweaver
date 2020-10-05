05e851966ee2a6597602070b9c095bc0c5193d39
JMS55 <47158642+JMS55@users.noreply.github.com>
2020-10-05 00:51:52 -0400

    Setup very basic pipeline

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,28 @@
-use std::iter;
+use obj::load_obj;
+use std::{iter, mem};
+use wgpu::util::{BufferInitDescriptor, DeviceExt};
 use wgpu::*;
-use winit::event::{Event, WindowEvent};
+use winit::event::{Event, VirtualKeyCode, WindowEvent};
 use winit::event_loop::{ControlFlow, EventLoop};
 use winit::window::WindowBuilder;
 
+#[repr(C)]
+#[derive(Copy, Clone)]
+struct Vertex {
+    position: [f32; 3],
+}
+unsafe impl bytemuck::Pod for Vertex {}
+unsafe impl bytemuck::Zeroable for Vertex {}
+
 fn main() {
+    // Create Window
     let event_loop = EventLoop::new();
     let window = WindowBuilder::new()
         .with_title("Meshweaver")
         .build(&event_loop)
         .unwrap();
 
+    // Setup WGPU
     let instance = Instance::new(BackendBit::PRIMARY);
     let surface = unsafe { instance.create_surface(&window) };
     let (device, queue) = pollster::block_on(async {
@@ -42,6 +54,74 @@ fn main() {
     };
     let mut swapchain = device.create_swap_chain(&surface, &swapchain_descriptor);
 
+    // Setup Rendering
+    let obj = load_obj::<_, _, u16>(&include_bytes!("../uvsphere.obj")[..]).unwrap();
+    let vertices = obj
+        .vertices
+        .into_iter()
+        .map(|vertex: obj::Vertex| Vertex {
+            position: vertex.position,
+        })
+        .collect::<Vec<Vertex>>();
+    let vertex_buffer = device.create_buffer_init(&BufferInitDescriptor {
+        label: None,
+        contents: bytemuck::cast_slice(&vertices),
+        usage: BufferUsage::VERTEX,
+    });
+    let index_buffer = device.create_buffer_init(&BufferInitDescriptor {
+        label: None,
+        contents: bytemuck::cast_slice(&obj.indices),
+        usage: BufferUsage::INDEX,
+    });
+    let indices_count = obj.indices.len() as u32;
+    let vertex_shader = device.create_shader_module(include_spirv!("../shaders/vert.spv"));
+    let fragment_shader = device.create_shader_module(include_spirv!("../shaders/frag.spv"));
+    let render_pipeline_layout = device.create_pipeline_layout(&PipelineLayoutDescriptor {
+        label: None,
+        bind_group_layouts: &[],
+        push_constant_ranges: &[],
+    });
+    let render_pipeline = device.create_render_pipeline(&RenderPipelineDescriptor {
+        label: None,
+        layout: Some(&render_pipeline_layout),
+        vertex_stage: ProgrammableStageDescriptor {
+            module: &vertex_shader,
+            entry_point: "main",
+        },
+        fragment_stage: Some(ProgrammableStageDescriptor {
+            module: &fragment_shader,
+            entry_point: "main",
+        }),
+        rasterization_state: Some(RasterizationStateDescriptor {
+            front_face: FrontFace::Ccw,
+            cull_mode: CullMode::Back,
+            clamp_depth: false,
+            depth_bias: 0,
+            depth_bias_slope_scale: 0.0,
+            depth_bias_clamp: 0.0,
+        }),
+        primitive_topology: PrimitiveTopology::TriangleList,
+        color_states: &[ColorStateDescriptor {
+            format: swapchain_descriptor.format,
+            alpha_blend: BlendDescriptor::REPLACE,
+            color_blend: BlendDescriptor::REPLACE,
+            write_mask: ColorWrite::ALL,
+        }],
+        depth_stencil_state: None,
+        vertex_state: VertexStateDescriptor {
+            index_format: IndexFormat::Uint16,
+            vertex_buffers: &[VertexBufferDescriptor {
+                stride: mem::size_of::<Vertex>() as BufferAddress,
+                step_mode: InputStepMode::Vertex,
+                attributes: &vertex_attr_array![0 => Float3],
+            }],
+        },
+        sample_count: 1,
+        sample_mask: !0,
+        alpha_to_coverage_enabled: false,
+    });
+
+    // Run EventLoop
     event_loop.run(move |event, _, control_flow| match event {
         Event::WindowEvent { event, .. } => match event {
             WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
@@ -55,6 +135,10 @@ fn main() {
                 swapchain_descriptor.height = new_inner_size.height;
                 swapchain = device.create_swap_chain(&surface, &swapchain_descriptor);
             }
+            WindowEvent::KeyboardInput { input, .. } => match input.virtual_keycode {
+                Some(VirtualKeyCode::Escape) => *control_flow = ControlFlow::Exit,
+                _ => {}
+            },
             _ => {}
         },
         Event::MainEventsCleared => {
@@ -65,7 +149,7 @@ fn main() {
             let mut encoder =
                 device.create_command_encoder(&CommandEncoderDescriptor { label: None });
             {
-                let _ = encoder.begin_render_pass(&RenderPassDescriptor {
+                let mut render_pass = encoder.begin_render_pass(&RenderPassDescriptor {
                     color_attachments: &[RenderPassColorAttachmentDescriptor {
                         attachment: &frame.view,
                         resolve_target: None,
@@ -76,6 +160,10 @@ fn main() {
                     }],
                     depth_stencil_attachment: None,
                 });
+                render_pass.set_pipeline(&render_pipeline);
+                render_pass.set_vertex_buffer(0, vertex_buffer.slice(..));
+                render_pass.set_index_buffer(index_buffer.slice(..));
+                render_pass.draw_indexed(0..indices_count, 0, 0..1);
             }
             queue.submit(iter::once(encoder.finish()));
         }
